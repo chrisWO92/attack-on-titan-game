be6bb4d6a7319fdb32fd45e345830b2e1e71a537
chrisWO92 <chrisworkingonline@gmail.com>
2023-04-08 20:36:21 -0300

    canva updates

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,76 +1,145 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./map.css";
-import mapBGsrc from '../../assets/map.png'
+import mapBGsrc from "../../assets/map.png";
 
-const Map = ({ charShowing, showCanvaMap, setShowCanvaMap, mapHiding, characters, characterIndex, pic }) => {
+let canvaVariables = {
+  x: 3,
+  y: 3,
+  width: 50,
+  height: 50,
+  speedX: 0,
+  speedY: 0,
+};
+
+const Map = ({
+  charShowing,
+  showCanvaMap,
+  setShowCanvaMap,
+  mapHiding,
+  characters,
+  characterIndex,
+  pic,
+}) => {
 
   const canvasRef = useRef();
+  const [canvas, setCanvas] = useState(canvaVariables)
+
+/*   setCanvas(canvas => ({
+    ...canvas,
+    ...canvaVariables
+  })) */
 
   let mapBG = new Image();
   mapBG.src = mapBGsrc;
 
   let charIMG = new Image();
-  charIMG.src = pic
+  charIMG.src = pic;
 
-  useEffect(() => {
-    const map = canvasRef.current
+ /*  const map = canvasRef.current;
+  const lienzo = map.getContext("2d"); */
+
+  const drawCanvas = () => {
+    const map = canvasRef.current;
     const lienzo = map.getContext("2d");
-    lienzo.clearRect(0, 0, map.width, map.height)
-    lienzo.fillStyle = '#fff'
-    //lienzo.fillRect(0, 60, 30, 20)
-    lienzo.drawImage(
-      mapBG,
-      0,
-      0,
-      map.width,
-      map.height
-    )
+    canvas.x = canvas.x + canvas.speedX;
+    canvas.y = canvas.y + canvas.speedY;
+    lienzo.clearRect(0, 0, map.width, map.height);
+    lienzo.drawImage(mapBG, 0, 0, map.width, map.height);
     lienzo.drawImage(
       charIMG,
-      20,
-      20,
-      50,
-      50
-    )
-    console.log(mapBG)
-    console.log(charIMG)
-  })
-
-  
-
-
-  /* const drawCanvas = () => {
-    lienzo.clearRect(0, 0, canvasRef.width, canvasRef.height);
-    lienzo.drawImage(mapBG, 0, 0, canvasRef.width, canvasRef.height);
-    lienzo.drawImage(
-      titanImageSelected,
-      titanSelected.x,
-      titanSelected.y,
-      titanSelected.width,
-      titanSelected.height
+      canvas.x,
+      canvas.y,
+      canvas.width,
+      canvas.height
     );
-  }; */
+  };
+
+  useEffect(() => {
+    drawCanvas()
+    /* const map = canvasRef.current;
+    const lienzo = map.getContext("2d");
+    canvas.x = canvas.x + canvas.speedX;
+    canvas.y = canvas.y + canvas.speedY;
+    lienzo.clearRect(0, 0, map.width, map.height);
+    lienzo.drawImage(mapBG, 0, 0, map.width, map.height);
+    lienzo.drawImage(
+      charIMG,
+      canvas.x,
+      canvas.y,
+      canvas.width,
+      canvas.height
+    );
+    console.log(canvas.x) */
+  });
+
+  const moveRight = () => {
+    setCanvas(canvas => ({
+      ...canvas,
+      speedX: 5,
+      speedY: 0
+    }))
+  };
+
+  const moveLeft = () => {
+    setCanvas(canvas => ({
+      ...canvas,
+      speedX: -5,
+      speedY: 0
+    }))
+  };
+
+  const moveUp = () => {
+    setCanvas(canvas => ({
+      ...canvas,
+      speedX: 0,
+      speedY: -5
+    }))
+  };
+
+  const moveDown = () => {
+    setCanvas(canvas => ({
+      ...canvas,
+      speedX: 0,
+      speedY: 5
+    }))
+  };
+
+  const stopMove = () => {
+    canvas.speedX = 0;
+    canvas.speedY = 0;
+  };
 
   return (
     <>
-      <div className={
-            charShowing 
-                ? "displayNone" 
-                : showCanvaMap
-                    ? "canvaMap"
-                    : "displayNone"
-                }
-                >
+      <div
+        className={
+          charShowing
+            ? "displayNone"
+            : showCanvaMap
+            ? "canvaMap"
+            : "displayNone"
+        }
+      >
         <canvas id="map" ref={canvasRef}>
           {/* {drawCanvas} */}
         </canvas>
         <div id="buttons">
-          <button id="up" className="move-button">Up</button>
-          <button id="down" className="move-button">Down</button>
-          <button id="right" className="move-button">Right</button>
-          <button id="left" className="move-button">Left</button>
+          <button id="up" className="move-button" onClick={moveUp}>
+            Up
+          </button>
+          <button id="down" className="move-button" onClick={moveDown}>
+            Down
+          </button>
+          <button id="right" className="move-button" onClick={moveRight}>
+            Right
+          </button>
+          <button id="left" className="move-button" onClick={moveLeft}>
+            Left
+          </button>
         </div>
-        <button id='map-button' onClick={mapHiding}>Continue</button>
+        <button id="map-button" onClick={mapHiding}>
+          Continue
+        </button>
       </div>
     </>
   );
